06edf55688132b385d12bef7c7bd71de7b5a36bc
Rafaqwert <rafael.rrodrigues@sptech.school>
2025-06-02 17:38:28 -0300

    feat: aws_api_gateway_stage

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -47,5 +47,10 @@ EOF
 resource "aws_api_gateway_deployment" "this" {
   depends_on = [aws_api_gateway_integration.put]
   rest_api_id = aws_api_gateway_rest_api.this.id
-  stage_name  = var.stage_name
+}
+
+resource "aws_api_gateway_stage" "this" {
+  rest_api_id          = aws_api_gateway_rest_api.this.id
+  deployment_id        = aws_api_gateway_deployment.this.id
+  stage_name           = var.stage_name
 }
