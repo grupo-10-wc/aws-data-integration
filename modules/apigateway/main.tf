d719c4941f4b27a6d7711a0f271f21e95853298c
Rafaqwert <rafael.rrodrigues@sptech.school>
2025-06-02 19:55:26 -0300

    fix: tenta ajustar recusrsos api gateway

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -23,6 +23,7 @@ resource "aws_api_gateway_method" "put" {
   resource_id   = aws_api_gateway_resource.filename.id
   http_method   = "PUT"
   authorization = "NONE"
+
   request_parameters = {
     "method.request.path.bucket"   = true
     "method.request.path.filename" = true
@@ -34,13 +35,40 @@ resource "aws_api_gateway_integration" "put" {
   resource_id             = aws_api_gateway_resource.filename.id
   http_method             = aws_api_gateway_method.put.http_method
   integration_http_method = "PUT"
-  type                    = "MOCK"
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:us-east-1:s3:path/{bucket}/{filename}"
+  credentials             = "arn:aws:iam::232922277588:role/LabRole"
+
+  request_parameters = {
+    "integration.request.path.bucket"   = "method.request.path.bucket"
+    "integration.request.path.filename" = "method.request.path.filename"
+  }
+
   request_templates = {
-    "application/json" = <<EOF
-{
-  "statusCode": 200
+    "application/json" = "$input.body"
+  }
+
 }
-EOF
+
+resource "aws_api_gateway_method_response" "put_200" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.filename.id
+  http_method = aws_api_gateway_method.put.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "put_200" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.filename.id
+  http_method = aws_api_gateway_method.put.http_method
+  status_code = aws_api_gateway_method_response.put_200.status_code
+
+  response_templates = {
+    "application/json" = ""
   }
 }
 
